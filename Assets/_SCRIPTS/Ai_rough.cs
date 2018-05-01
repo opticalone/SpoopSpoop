2df841181b926cc8404b535507f32429b1f21e21
Michael Kaplor <s179059@students.aie.edu.au>
2018-05-01 15:04:48 -0700

    finally figured out what the game should be

---
+++ Assets/_SCRIPTS/Ai_rough.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Ai_rough : MonoBehaviour 
+{
+	public Transform target;
+	private NavMeshAgent agent;
+	Vector3 dir;
+	float viewRange;
+
+
+	void Start()
+	{
+		agent = GetComponent<NavMeshAgent> ();
+		dir = transform.TransformDirection (Vector3.forward);
+	}
+
+
+	void Update()
+	{
+
+	NavMeshHit hit;
+
+
+
+		if (!agent.Raycast (target.position, out hit)) 
+		{
+			
+		}
+			
+		
+
+	}
+
+
+}
